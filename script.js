0b7266b76efef4081a490a82437ad5e21f1bebc5
Allyson <jandreyallyson@gmail.com>
2022-05-18 19:02:42 -0300

    Implementação do JavaScript

---
+++ script.js
@@ -0,0 +1,63 @@
+//recurso: viacep.com.br/ws/01001000/json/
+let cep = document.querySelector('#cep');
+let rua = document.querySelector('#rua');
+let bairro = document.querySelector('#bairro');
+let cidade = document.querySelector('#cidade');
+let uf = document.querySelector('#uf'); 
+let ibge = document.querySelector('#ibge');
+let ddd = document.querySelector('#ddd');
+
+cep.value = '01001000';
+
+cep.addEventListener('blur', function(e) {
+    let cep = e.target.value;
+    let script  = document.createElement('script');
+    script.src = 'https://viacep.com.br/ws/'+cep+'/json/?callback=informacoesFormulario';
+    document.body.appendChild(script);
+
+    function checarInput();
+});
+
+function informacoesFormulario(resultado) {
+    
+    if("erro" in resultado) {
+        alert('CEP não encontrado');
+        return;
+    }
+
+    console.log(resultado);
+    rua.value = resultado.logradouro;
+    bairro.value = resultado.bairro;
+    cidade.value = resultado.localidade;
+    uf.value = resultado.uf;
+    ibge.value = resultado.ibge;
+    ddd.value = resultado.ddd;
+}
+
+function checarInput() {
+    const cepValor = cep.value;
+
+    if (cepValor == "") {
+        setarErro(username, "Insira um cep válido");
+    } else if (cepValor.length > 9) {
+        setarErro(username, "Um CEP não tem mais que 9 dígitos")
+    } else {
+        setarSucesso(username);
+    }
+}
+
+function setarErro(input, mensagem) {
+    const conteudoFormulario = input.parentElement;
+    const small = conteudoFormulario.querySelector('small');
+    
+
+    small.innerText = mensagem;
+
+    conteudoFormulario.className = "conteudo-formulario erro";
+}
+
+function setarSucesso(input) {
+    const conteudoFormulario = input.parentElement;
+
+    conteudoFormulario.className = "conteudo-formulario sucesso";
+}
