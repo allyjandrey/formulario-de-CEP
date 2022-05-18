fcf1e3e58e782052888df8a853984fc7c0c42bbe
Allyson <jandreyallyson@gmail.com>
2022-05-18 20:43:55 -0300

    mais funcionaliades no js e correcao de erros

--- script.js
+++ script.js
@@ -7,21 +7,56 @@ let uf = document.querySelector('#uf');
 let ibge = document.querySelector('#ibge');
 let ddd = document.querySelector('#ddd');
 
-cep.addEventListener('blur', function(e) {
+cep.addEventListener('blur', async function(e) {
     let cep = e.target.value;
     let script  = document.createElement('script');
+    const small = document.getElementById('small');
+    const CEP = document.getElementById('cep');
+    const inputt = document.querySelectorAll('input');
+
     script.src = 'https://viacep.com.br/ws/'+cep+'/json/?callback=informacoesFormulario';
     document.body.appendChild(script);
+
+    if(CEP.value === '') {
+        CEP.style.borderColor = "#C02F1F"
+        small.innerHTML = "Essa informação é obrigatória"
+    }
 });
 
-function informacoesFormulario(resultado) {
+async function informacoesFormulario(resultado) {
+    const small = document.querySelector('small');
+    const inputt = document.querySelectorAll('input')
+    small.innerHTML = ""
     
+    if("erro" in resultado){
+        cep.style.borderColor = ""
+        small.innerHTML = "O CEP inserido é inválido"
+        inputt.value = ""
+    } else {
+        cep.style.borderColor = "#2ECC71"
+        await delay(2);
 
-    console.log(resultado);
     rua.value = resultado.logradouro;
     bairro.value = resultado.bairro;
     cidade.value = resultado.localidade;
     uf.value = resultado.uf;
     ibge.value = resultado.ibge;
     ddd.value = resultado.ddd;
+    }
+}
+
+function delay(i){
+
+    return new Promise(function(resolve){
+
+        cep.value = 'loading...'
+        rua.value = 'loading...'
+        bairro.value = 'loading...'
+        cidade.value = 'loading...'
+        uf.value = 'loading...'
+        ibge.value = 'loading...'
+        ddd.value = 'loading...'
+
+        setTimeout(resolve,i*1000);
+    });
 }
